492e78de1a5e86d68ea31a30540c5b05d535949c
anagha-04 <anaghanilkumar04@gmail.com>
2025-12-13 07:20:28 +0530

    get post sett

--- API/views.py
+++ API/views.py
@@ -5,23 +5,26 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
-from API.serializers import UserRegisterSerializer
+from API.serializers import UserSerializer
 
 class UserRegisterView(APIView):
 
-    def get(self, request):
+    def get(self, request): 
+
         users = User.objects.all()
-        serializer = UserRegisterSerializer(users, many=True)  
+
+        serializer = UserSerializer(users, many=True)
+
         return Response(serializer.data)
     
-    # def post(self,request):
+    def post(self,request):
 
-    #     serializer = UserRegisterSerializer(data = request.data)
+        serializer = UserSerializer(data = request.data)
 
-    #     if serializer.is_valid():
+        if serializer.is_valid():
 
-    #         serializer.save()    
+            serializer.save()    
 
-    #         return Response(status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
         
-    #     return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
